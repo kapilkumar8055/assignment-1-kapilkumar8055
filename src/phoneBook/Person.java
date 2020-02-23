4a31b592dc187773ba4fc58796baf7fadf008f8f
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-02-23 22:13:02 +0530

    create getter and setter for fields

--- src/phoneBook/Person.java
+++ src/phoneBook/Person.java
@@ -6,4 +6,35 @@ public class Person {
     private long[] number = new long[3];
     private String email;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long[] getNumber() {
+        return number;
+    }
+
+    public void setNumber(long[] number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
