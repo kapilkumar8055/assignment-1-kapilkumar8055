7cbc776aef395a8974e76804649d4648aace17a6
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-02-23 22:10:20 +0530

    create fields

--- src/phoneBook/Main.java
+++ src/phoneBook/Main.java
@@ -3,5 +3,6 @@ package phoneBook;
 public class Main {
     public static void main(String[] args) {
 
+
     }
 }
---
+++ src/phoneBook/Person.java
@@ -0,0 +1,8 @@
+package phoneBook;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private long[] number = new long[3];
+    private String email;
+}
