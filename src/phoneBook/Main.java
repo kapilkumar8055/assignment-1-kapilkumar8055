91fa825349357f15b5b93c2321bc1a7b6312b6b1
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-02-24 22:17:34 +0530

    print the statements for add new contact,delete,view all contact ,serach for contact and exit

--- src/phoneBook/Main.java
+++ src/phoneBook/Main.java
@@ -7,6 +7,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         boolean cases = true;
         Impementation impementation = new Impementation();
-
+        do {
+            System.out.println("\n" + "Press 1 to add a new contact ");
+            System.out.println("Press 2 to view all contacts ");
+            System.out.println("Press 3 to search for a contact ");
+            System.out.println("Press 4 to delete a contact ");
+            System.out.println("Press 5 to exit program \n");
+        }
     }
 }
