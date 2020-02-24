24040eb9be40cb638e7ac8b07a39e08ad49dcce3
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-02-24 20:04:17 +0530

    create methods

--- src/phoneBook/Impementation.java
+++ src/phoneBook/Impementation.java
@@ -1,8 +1,35 @@
 package phoneBook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Impementation implements Interface {
     Scanner scanner = new Scanner(System.in);
     CheckUsingRegex checkUsingRegex = new CheckUsingRegex();
+    ArrayList<Person> contact = new ArrayList<>();
+
+    @Override
+    public void addNewContact() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
+
+    @Override
+    public void viewAllContact() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
 }
