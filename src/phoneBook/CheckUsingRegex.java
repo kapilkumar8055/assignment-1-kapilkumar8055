e24b55100bccbe3604d547925e75aaa9ca17fb6c
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-02-24 19:27:06 +0530

    create method for check first name

--- src/phoneBook/CheckUsingRegex.java
+++ src/phoneBook/CheckUsingRegex.java
@@ -1,4 +1,12 @@
 package phoneBook;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckUsingRegex {
+    public boolean isFirstName(String firstName) {
+        Pattern pattern = Pattern.compile("([A-z]{3,15})");
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 }
