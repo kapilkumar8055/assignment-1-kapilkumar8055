fad8eaf9713b7636074c36f1817d11d089240f86
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-02-24 19:58:34 +0530

    create object of CheckUsingregex class

--- src/phoneBook/Impementation.java
+++ src/phoneBook/Impementation.java
@@ -4,4 +4,5 @@ import java.util.Scanner;
 
 public class Impementation implements Interface {
     Scanner scanner = new Scanner(System.in);
+    CheckUsingRegex checkUsingRegex = new CheckUsingRegex();
 }
